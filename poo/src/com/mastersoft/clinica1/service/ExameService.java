ed12e2d19dcd72f1e0c0b85b130f8885cfede0e9
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-20 16:14:16 -0300

    adiciona método salvarExame na classe ExameService

--- poo/src/com/mastersoft/clinica1/service/ExameService.java
+++ poo/src/com/mastersoft/clinica1/service/ExameService.java
@@ -1,5 +1,10 @@
 package com.mastersoft.clinica1.service;
 
+import com.mastersoft.clinica1.model.Exame;
+
 public class ExameService {
+    void salvarExame(Exame exame) {
+        // Implementação do método para salvar um exame
+    }
 
 }
